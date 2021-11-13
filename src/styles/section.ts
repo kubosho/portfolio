330cc46d12a6e8b0c449a303b7836bf3e22dce1d
Shota Kubota <ta2@o2p.jp>
2021-11-13 22:02:13 +0900

    feat(sytles/section): add top margin

--- src/styles/section.ts
+++ src/styles/section.ts
@@ -7,12 +7,12 @@ export const sectionStyles = css`
   }
 
   .section-title {
-    margin: 0 0 8px;
+    margin: 8px 0;
     font-size: 24px;
   }
   @media (min-width: 725px) {
     .section-title {
-      margin: 0 0 16px;
+      margin: 16px 0;
       font-size: 32px;
     }
   }
