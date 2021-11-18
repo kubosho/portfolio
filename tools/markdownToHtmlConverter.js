f64d4fcc481555ec8c571bb4e3f48aa6e99e0357
Shota Kubota <ta2@o2p.jp>
2021-11-18 23:01:06 +0900

    feat(markdownToHtmlConverter): split of process of convert a contents into HTML strings

--- tools/markdownToHtmlConverter.js
+++ tools/markdownToHtmlConverter.js
@@ -10,23 +10,33 @@ const SRC_DIR = './texts';
 const DIST_DIR = './build';
 const SOURCE = `${SRC_DIR}/**/*.md`;
 
+async function convertToHtmlString(filename) {
+  const contents = await readFile(filename, { encoding: 'utf-8' });
+  const htmlString = markdown.parse(contents);
+
+  if (filename.includes('skill-sets')) {
+    const modifiedHtmlString = htmlString.replaceAll(/<(\/)?h2>/g, '<$1dt>').replaceAll(/<(\/)?p>/g, '<$1dd>');
+    return `<dl>${modifiedHtmlString}</dl>`;
+  }
+
+  return htmlString;
+}
+
 async function main() {
   const markdownFileNameList = await asyncGlob(SOURCE);
 
   markdownFileNameList.forEach(async (filename) => {
-    const distFilePath = filename.replace(SRC_DIR, DIST_DIR).replace('.md', '.html');
-    const distDirName = path.dirname(distFilePath);
-
-    const contents = await readFile(filename, { encoding: 'utf-8' });
-    const htmlString = markdown.parse(contents);
+    const htmlString = await convertToHtmlString(filename);
 
     try {
+      const distFilePath = filename.replace(SRC_DIR, DIST_DIR).replace('.md', '.html');
+      const distDirName = path.dirname(distFilePath);
+
       await mkdir(distDirName, { recursive: true });
+      await writeFile(distFilePath, htmlString);
     } catch (err) {
       console.error(err);
     }
-
-    await writeFile(distFilePath, htmlString);
   });
 }
 
